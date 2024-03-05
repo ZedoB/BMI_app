85a43c608a1c3abb2ae813656614f95030189c03
ZEDO B <zedoaboda@gmail.com>
2024-03-05 18:57:03 +0200

    - Make some fixes

--- lib/result.dart
+++ lib/result.dart
@@ -9,9 +9,8 @@ class Result extends StatelessWidget {
 
   String get resultPhrase{
     String resultText = '';
-    if(result >= 30) {
-      resultText = 'Obese';
-    } else if(result >= 25 && result < 30) resultText = 'Overweight';
+    if(result >= 30) {resultText = 'Obese';}
+    else if(result >= 25 && result < 30) resultText = 'Overweight';
     else if(result >= 18.5 && result < 25) resultText = 'Normal';
     else resultText = 'Thin';
     return resultText;
@@ -20,7 +19,9 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal,
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: const Text('Result'),
       ),
       body: SafeArea(
